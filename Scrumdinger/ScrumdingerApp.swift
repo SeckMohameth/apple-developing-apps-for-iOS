b6daf6939512c4fbaf30a6abc50e8d5912590ef7
SeckMohameth <mohamethseck34@gmail.com>
2024-01-20 17:43:56 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Mohameth Seck on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
