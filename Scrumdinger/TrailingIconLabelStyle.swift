911a90c3f6ce5dbaa294f2db4b22c097acf9a96a
SeckMohameth <mohamethseck34@gmail.com>
2024-01-23 00:25:24 -0500

    added accessibility to the card view

--- Scrumdinger/TrailingIconLabelStyle.swift
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -5,4 +5,19 @@
 //  Created by Mohameth Seck on 1/22/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+    
+}
+
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
