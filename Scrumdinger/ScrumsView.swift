6c7394cffd36bdaf3df268535cfd108ba8bd012b
SeckMohameth <mohamethseck34@gmail.com>
2024-01-23 23:28:36 -0500

    completed chapter 2 - Views

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Mohameth Seck on 1/23/24.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrumsView()
+}
